0ce626172cc227c65aea7348375522fc068ef06b
EK <qxjj7ay@163.com>
2019-06-14 17:48:13 +0800

    'reset'

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      {{message}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data () {
+          return {
+            message: "hello world"
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

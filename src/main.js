6b6ec04eef83f0d2417d07bf83b8743481b31350
EK <qxjj7ay@163.com>
2019-06-16 17:31:36 +0800

    'vuex'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import ElementUI from 'element-ui'
@@ -16,6 +17,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })

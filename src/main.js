2fc338db72de8d09ad74e6f6725f83cecbe999c0
EK <qxjj7ay@163.com>
2019-06-16 18:05:50 +0800

    'fastclick'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
 import store from './store'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
@@ -12,6 +13,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI, { size: 'small', zIndex: 3000 })
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({

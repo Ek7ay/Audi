0d23c6d054031ea3a3540229167b30c8da99bbbd
EK <qxjj7ay@163.com>
2019-06-24 15:18:21 +0800

    'iconfinish'

--- src/pages/index/Icons.vue
+++ src/pages/index/Icons.vue
@@ -1,17 +1,10 @@
 <template>
     <div class="icons">
       <swiper :options="swiperOption">
-        <swiper-slide>
-          <div class="icon">
-            <img :src="rec"/>
-            <p>充值中心</p>
-          </div>
-        </swiper-slide>
-
-        <swiper-slide>
-          <div class="icon">
-            <img :src="rec"/>
-            <p>充值中心</p>
+        <swiper-slide v-for="(page, index) of pages" :key="index">
+          <div class="icon" v-for="item of page" :key="item.id">
+            <img :src="item.imgUrl"/>
+            <p>{{item.desc}}</p>
           </div>
         </swiper-slide>
       </swiper>
@@ -23,8 +16,85 @@
         name: "Icons",
         data () {
           return {
-            rec: require("../../assets/img/index/icons/Rechargecenter@2x.png"),
             swiperOption: {},
+            iconList: [{
+              id: "001",
+              imgUrl: require("../../assets/img/index/icons/Rechargecenter@2x.png"),
+              desc: "充值中心"
+            },{
+              id: "002",
+              imgUrl: require("../../assets/img/index/icons/Tradingmarket@2x.png"),
+              desc: "交易市场"
+            },{
+              id: "003",
+              imgUrl: require("../../assets/img/index/icons/Parkingservice@2x.png"),
+              desc: "停车服务"
+            },{
+              id: "004",
+              imgUrl: require("../../assets/img/index/icons/Carwashingservice@2x.png"),
+              desc: "洗车服务"
+            },{
+              id: "005",
+              imgUrl: require("../../assets/img/index/icons/Penguinhealth@2x.png"),
+              desc: "企鹅健康"
+            },{
+              id: "006",
+              imgUrl: require("../../assets/img/index/icons/Driver'slicensedifference@2x.png"),
+              desc: "驾照查分"
+            },{
+              id: "007",
+              imgUrl: require("../../assets/img/index/icons/Illegalinvestigation@2x.png"),
+              desc: "违章查办"
+            },{
+              id: "008",
+              imgUrl: require("../../assets/img/index/icons/Finesremitted@2x.png"),
+              desc: "罚单代缴"
+            },{
+              id: "009",
+              imgUrl: require("../../assets/img/index/icons/Drivingservice@2x.png"),
+              desc: "代驾服务"
+            },{
+              id: "010",
+              imgUrl: require("../../assets/img/index/icons/Carwarranty@2x.png"),
+              desc: "爱车保养"
+            },{
+              id: "011",
+              imgUrl: require("../../assets/img/index/icons/Vehicleinspection@2x.png"),
+              desc: "车辆检测"
+            },{
+              id: "012",
+              imgUrl: require("../../assets/img/index/icons/Highincidenceofillegalactivities@2x.png"),
+              desc: "违章高发"
+            },{
+              id: "013",
+              imgUrl: require("../../assets/img/index/icons/Rescueservice@2x.png"),
+              desc: "救援服务"
+            },{
+              id: "014",
+              imgUrl: require("../../assets/img/index/icons/Carvaluation@2x.png"),
+              desc: "爱车估计"
+            },{
+              id: "015",
+              imgUrl: require("../../assets/img/index/icons/Claimsinquiry@2x.png"),
+              desc: "出险理赔"
+            },{
+              id: "016",
+              imgUrl: require("../../assets/img/index/icons/vehiclemaintenance@2x.png"),
+              desc: "车辆保养"
+            },]
+          }
+        },
+        computed: {
+          pages () {
+            const pages = [];
+            this.iconList.forEach((item, index) => {
+              const page = Math.floor(index / 8);
+              if (!pages[page]) {
+                pages[page] = [];
+              }
+              pages[page].push(item)
+            });
+            return pages;
           }
         }
     }
@@ -52,8 +122,5 @@
       p
         font-size .16rem
         margin-top 10%
-
-
-
-
+        ellipsis()
 </style>

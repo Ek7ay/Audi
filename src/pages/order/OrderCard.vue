5cb000418990dfc93756d438e393d82fff3abd65
EK <qxjj7ay@163.com>
2019-07-12 15:31:55 +0800

    'order-finish'

---
+++ src/pages/order/OrderCard.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="order-card">
+    <div class="top">
+      <div>
+        <img :src="cardInfo.imgUrl"/>
+        <span>{{cardInfo.price}}&nbsp;站豆</span>
+      </div>
+      <div>
+        <p>{{cardInfo.name}}</p>
+        <p>订单号：{{cardInfo.orderNum}}</p>
+        <p>{{cardInfo.time}}</p>
+      </div>
+      <div>
+        <p>{{cardInfo.price}}</p>
+        <p>{{tradingState}}</p>
+      </div>
+    </div>
+    <div class="bottom">
+      <span>共{{cardInfo.num}}件商品</span>
+      合计：<span>{{cardInfo.price}}</span>&nbsp;站豆
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+        info: {
+          type: Object
+        }
+    },
+    data () {
+        return {
+            // cardInfo: {
+            //     imgUrl: require("../../assets/img/order/Vehicleinspection@2x.png"),
+            //     name: "车辆事故检测",
+            //     orderNum: "48814545488715",
+            //     time: "2018-8-15 11:24:00",
+            //     price: 149,
+            //     num: 1,
+            //     state: "01",
+            // }
+            cardInfo: this.info
+        }
+    },
+    computed: {
+        tradingState () {
+            if (this.cardInfo.state === "01") { return "交易成功" }
+            if (this.cardInfo.state === "02") { return "交易失败" }
+        }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .order-card
+    background-color $bgColor
+    margin-bottom .20rem
+    .top
+      height 2.1rem
+      display flex
+      justify-content space-between
+      padding 0 .3rem
+      border-bottom .01rem solid #efefef
+      div:first-child
+        position relative
+        img
+          width 2.1rem
+          margin-top .30rem
+        span
+          position absolute
+          top .41rem
+          left .36rem
+          font-size .24rem
+          color $bgColor
+      div:nth-child(2)
+        flex 1
+        color #2f2f2f
+        position relative
+        white-space nowrap
+        p:first-child
+          font-size .32rem
+          position absolute
+          top .46rem
+          left .20rem
+        p:nth-child(2)
+          font-size .26rem
+          position absolute
+          top 1.08rem
+          left .20rem
+        p:last-child
+          font-size .24rem
+          position absolute
+          top 1.54rem
+          left .20rem
+          color #787878
+      div:last-child
+        text-align right
+        p:first-child
+          font-size .36rem
+          color #2f2f2f
+          margin-top .32rem
+        p:last-child
+          font-size .24rem
+          color #fe4437
+          margin-top .84rem
+    .bottom
+      font-size .22rem
+      height .70rem
+      line-height .70rem
+      padding 0 .30rem
+      text-align right
+      color #787878
+      span:first-child
+        margin-right .28rem
+      span:last-child
+        font-size .28rem
+        color #fe4437
+</style>
--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -1,14 +1,159 @@
 <template>
     <div class="order">
-        hello world
+        <order-card
+            v-for="item of cardInfo"
+            :info="item"
+            :key="item.id"
+        />
     </div>
 </template>
 
 <script>
+    import OrderCard from './OrderCard'
     export default {
+        components: {
+            OrderCard
+        },
         data () {
             return {
-
+                cardInfo: [
+                  {
+                      id: "001",
+                      imgUrl: require("../../assets/img/order/Vehicleinspection@2x.png"),
+                      name: "车辆事故检测",
+                      orderNum: "48814545458715",
+                      time: "2018-8-15 11:25:00",
+                      price: 149,
+                      num: 1,
+                      state: "01",
+                  },{
+                      id: "002",
+                      imgUrl: require("../../assets/img/order/Carwarranty@2x.png"),
+                      name: "爱车保修检测",
+                      orderNum: "488145454545",
+                      time: "2018-8-15 11:24:00",
+                      price: 99,
+                      num: 9,
+                      state: "01",
+                  },{
+                      id: "003",
+                      imgUrl: require("../../assets/img/order/Drivingservice@2x.png"),
+                      name: "代驾服务",
+                      orderNum: "4881454545225",
+                      time: "2018-8-15 02:24:36",
+                      price: 700,
+                      num: 1,
+                      state: "01",
+                  },{
+                      id: "004",
+                      imgUrl: require("../../assets/img/order/Carwashingservice@2x.png"),
+                      name: "大狗洗车服务",
+                      orderNum: "4881454545441",
+                      time: "2018-8-15 08:24:22",
+                      price: 290,
+                      num: 30,
+                      state: "01",
+                  },{
+                      id: "005",
+                      imgUrl: require("../../assets/img/order/Carvaluation@2x.png"),
+                      name: "爱车估值",
+                      orderNum: "488145454155",
+                      time: "2018-8-15 11:36:15",
+                      price: 15,
+                      num: 10,
+                      state: "01",
+                  },{
+                      id: "006",
+                      imgUrl: require("../../assets/img/order/Insuranceclaim@2x.png"),
+                      name: "出险理赔",
+                      orderNum: "488144981196",
+                      time: "2018-8-15 11:24:35",
+                      price: 15,
+                      num: 1,
+                      state: "01",
+                  },{
+                      id: "007",
+                      imgUrl: require("../../assets/img/order/Highincidenceofviolation@2x.png"),
+                      name: "违章高发",
+                      orderNum: "4881454549698",
+                      time: "2018-8-15 10:24:24",
+                      price: 15,
+                      num: 1,
+                      state: "01",
+                  },{
+                      id: "008",
+                      imgUrl: require("../../assets/img/order/Penguinhealth@2x.png"),
+                      name: "企鹅健康",
+                      orderNum: "4881454579",
+                      time: "2018-8-15 06:24:29",
+                      price: 1000,
+                      num: 5,
+                      state: "01",
+                  },{
+                      id: "009",
+                      imgUrl: require("../../assets/img/order/Transferofstandingbean@2x.png"),
+                      name: "站豆转让",
+                      orderNum: "48814569849",
+                      time: "2018-8-15 09:35:00",
+                      price: 100,
+                      num: 1,
+                      state: "01",
+                  },{
+                      id: "010",
+                      imgUrl: require("../../assets/img/order/illegalquery@2x.png"),
+                      name: "违章查询",
+                      orderNum: "4881454556894",
+                      time: "2018-8-15 09:24:12",
+                      price: 10,
+                      num: 1,
+                      state: "01",
+                  },{
+                      id: "011",
+                      imgUrl: require("../../assets/img/order/Top-upoilcard@2x.png"),
+                      name: "中国石油卡油卡充值",
+                      orderNum: "488145499769",
+                      time: "2018-8-15 09:24:36",
+                      price: 100,
+                      num: 60,
+                      state: "01",
+                  },{
+                      id: "012",
+                      imgUrl: require("../../assets/img/order/Prepaidrecharge@2x.png"),
+                      name: "话费充值",
+                      orderNum: "488145465987",
+                      time: "2018-8-15 11:24:45",
+                      price: 10,
+                      num: 1,
+                      state: "01",
+                  },{
+                      id: "013",
+                      imgUrl: require("../../assets/img/order/Etcpstop@2x.png"),
+                      name: "ETCP停车券",
+                      orderNum: "4881454987962",
+                      time: "2018-8-15 22:22:10",
+                      price: 10,
+                      num: 100,
+                      state: "01",
+                  },{
+                      id: "014",
+                      imgUrl: require("../../assets/img/order/Refuelingprepaidcardpurchase@2x.png"),
+                      name: "中国石化加油充值卡采购",
+                      orderNum: "488145469789",
+                      time: "2018-8-15 23:24:53",
+                      price: 100000,
+                      num: 25,
+                      state: "01",
+                  },{
+                      id: "015",
+                      imgUrl: require("../../assets/img/order/CarWash@2x.png"),
+                      name: "洗车卡交易",
+                      orderNum: "488145497986",
+                      time: "2018-8-15 20:00:00",
+                      price: 180,
+                      num: 800,
+                      state: "01",
+                  }
+                ]
             }
         }
     }
